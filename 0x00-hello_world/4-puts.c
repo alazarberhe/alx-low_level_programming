b1f45976a2de7aa9f3188d8da5b7a2b40545cc6a
alazarberhe <alazarberhe4695@gmail.com>
2022-04-07 10:58:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * * main - Write '"Programming is like building a multilingual puzzle'
- * *Return: Always 0 (success)
- * */
+ * main - Prints out a string to stdout.
+ *
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
