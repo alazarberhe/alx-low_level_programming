6a0b675ec6ed1cf45bf44e95a673f2e8d3288750
root <root@samuel.localdomain>
2022-04-10 22:40:46 -1200

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-int i;
+		int num;
 
-for (i = 0; i < 9; ++i)
-putchar('0' + i);
+			for (num = 0; num < 10; num++)
+						putchar((num % 10) + '0');
 
-putchar('\n');
+				putchar('\n');
 
-return (0);
+					return (0);
 }
 
