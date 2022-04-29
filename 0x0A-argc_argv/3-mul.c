b6538a1520713f5746da82651750cc8f041536d1
root <root@DESKTOP-9JI2CF9.localdomain>
2022-04-29 07:47:48 -0700

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - prints multiplication
- * of two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: returns 0
- */
 
+/**
+ * main - multiplies two numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-int i, j, mul;
+int i, val = 1;
 
-if (argc <= 2)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-mul = i * j;
-
-printf("%d\n", mul);
+for (i = 1; i < argc; i++)
+{
+val *= atoi(argv[i]);
+}
+printf("%d\n", val);
 return (0);
 }
